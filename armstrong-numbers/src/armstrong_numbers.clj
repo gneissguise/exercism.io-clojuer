343291ff2f98a24ffd69be4714ff262d4522c1e1
Justin Frost <frostjust@gmail.com>
2021-04-11 23:56:51 -0700

    Added armstrong numbers

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -1,46 +1,28 @@
-(ns armstrong-numbers)
-(require '[clojure.string :as string])
-
-;;; Note: this could be simplified and contained in fewer functions,
-;;; but I am fairly new to clojure and I like to break things out!
-
-(defn num-split
-  "Takes a number, and converts it to a collection of str digits.  1234 -> ['1' '2' '3' '4']"
-  [num] 
-  (string/split (str num) #"(?=[0-9])")
-)
-
-(defn parse-int
-  "Converts from string to int using the java Integer class.  '1' -> 1"
-  [s]
-  (Integer/parseInt (re-find #"\A-?\d+" s))
-)
+(ns armstrong-numbers
+  (:require [clojure.string :as string]))
 
 (defn **
   "Exponent operator (** 2 2) -> 2 ^ 2 -> 4"
   [x n]
-  (reduce * (repeat n x))
-)
+  (reduce * (repeat n x)))
 
-(defn int-coll
-  "Takes a str collection and converts it to an int collection"
+(defn parse-ints
+  "Maps a coll of strs to their respective int form"
   [coll]
-  (map parse-int coll)
-)
+  (map #(Integer/parseInt %) coll))
 
-(defn map-exp
-  "Takes a number, breaks each digit into an individual item in a collection, applies an exponent of the number of digits, then adds each result together creating an Armstrong number"
-  [num]
-  (->> num
-    (num-split)
-    (int-coll)
-    (map (fn [i] (** i (count (str num)))))
-    (reduce +)
-  )
-)
+(defn parse-armstrong
+  "Converts num->str, splits by char, converts each char to int, 
+   maps the exponent value and adds total via reduce. The result 
+   is an Armstrong Number."
+  [n]
+  (let [s (str n)
+        ints (parse-ints (string/split s #""))
+        len (count ints)
+        exp #(** % len)]
+    (reduce + (map exp ints))))
 
-(defn armstrong? 
+(defn armstrong?
   "Tests to see if a number is an Armstrong number"
-  [num]   
-  (= num (map-exp num))
-)
+  [num]
+  (= (parse-armstrong num) num))
