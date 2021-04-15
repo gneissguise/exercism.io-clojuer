c05c5c4fbab10039a1b40bf830640cd617cd8258
Justin Frost <frostjust@gmail.com>
2021-04-14 18:44:54 -0700

    Update to Bob per mentor advice

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,39 +1,31 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn capitalize-word
-  [s]
-  (str/replace s #"[a-z]" str/capitalize))
-
-(defn is-alpha?
+(defn alpha?
   [s]
   (re-find #"[A-Za-z]+" s))
 
-(defn is-a-question?
+(defn question?
   [s]
   (str/ends-with? s "?"))
 
-(defn is-yelling?
+(defn yelling?
   [s]
   (and
-   (is-alpha? s)
-   (= (capitalize-word s) s)))
+   (alpha? s)
+   (= (str/upper-case s) s)))
 
-(defn is-a-yelling-question?
+(defn yelling-question?
   [s]
   (and
-   (is-yelling? s)
-   (is-a-question? s)))
-
-(defn is-saying-nothing?
-  [s]
-  (str/blank? s))
+   (yelling? s)
+   (question? s)))
 
 (defn response-for [s]
   (let [processed-str (str/trim-newline (str/trimr s))]
     (cond
-      (is-a-yelling-question? processed-str) "Calm down, I know what I'm doing!"
-      (is-a-question? processed-str) "Sure."
-      (is-yelling? processed-str) "Whoa, chill out!"
-      (is-saying-nothing? processed-str) "Fine. Be that way!"
+      (yelling-question? processed-str) "Calm down, I know what I'm doing!"
+      (question? processed-str) "Sure."
+      (yelling? processed-str) "Whoa, chill out!"
+      (str/blank? processed-str) "Fine. Be that way!"
       :else "Whatever.")))
